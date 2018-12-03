512c03de00180b808c74fbd74043a652dccd167b
Armin Rigo <armin.rigo@gmail.com>
2018-12-03 19:32:41 +0200

    ZShift offset doesn't seem to work when drawing lines

---
+++ Assets/LinePriority/LinePriority.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinePriority : MonoBehaviour
+{
+    public Vector3 start, stop;
+
+    private void Start()
+    {
+        var mesh = new Mesh();
+        mesh.vertices = new Vector3[] { start, stop };
+        mesh.SetIndices(new int[] { 0, 1 }, MeshTopology.Lines, 0);
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
+}
