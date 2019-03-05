df3ef8f63dbdeeccc7533359f38b196991811a85
Armin Rigo <arigo@tunes.org>
2019-03-05 13:31:10 +0100

    A Window to elsewhere.

---
+++ Assets/WindowToElsewhere/WindowToElsewhere.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using BaroqueUI;
+
+
+public class WindowToElsewhere : MonoBehaviour
+{
+    public Camera secondCamera;
+
+    RenderTexture rt_left, rt_right;
+
+
+    /* NOTE: clean code, but only works if Edit -> Project Settings -> Player -> XR Settings ->
+     * Stereo Rendering Method is set to Multi Pass.
+     */
+
+    private void OnWillRenderObject()
+    {
+        if (rt_right == null)
+        {
+            rt_left = new RenderTexture(XRSettings.eyeTextureDesc);
+            rt_right = new RenderTexture(XRSettings.eyeTextureDesc);
+            GetComponent<Renderer>().material.SetTexture("_LeftTex", rt_left);
+            GetComponent<Renderer>().material.SetTexture("_RightTex", rt_right);
+
+            var cam = Baroque.GetHeadTransform().GetComponent<Camera>();
+            secondCamera.targetTexture = rt_left;
+            secondCamera.fieldOfView = cam.fieldOfView;
+            secondCamera.farClipPlane = cam.farClipPlane;
+            secondCamera.nearClipPlane = cam.nearClipPlane;
+        }
+
+        Vector3 sc_pos = secondCamera.transform.position;
+        Quaternion sc_rot = secondCamera.transform.rotation;
+        try
+        {
+            /* we want a matrix that would map 'transform.position/rotation' to
+             * 'secondCamera.transform.position/rotation'
+             */
+            Matrix4x4 mat = secondCamera.transform.localToWorldMatrix * transform.worldToLocalMatrix;
+
+            secondCamera.transform.position = mat.MultiplyPoint(InputTracking.GetLocalPosition(XRNode.LeftEye));
+            secondCamera.transform.rotation = mat.rotation * InputTracking.GetLocalRotation(XRNode.LeftEye);
+            secondCamera.projectionMatrix = Camera.main.GetStereoProjectionMatrix(Camera.StereoscopicEye.Left);
+            secondCamera.targetTexture = rt_left;
+            secondCamera.Render();
+
+            secondCamera.transform.position = mat.MultiplyPoint(InputTracking.GetLocalPosition(XRNode.RightEye));
+            secondCamera.transform.rotation = mat.rotation * InputTracking.GetLocalRotation(XRNode.RightEye);
+            secondCamera.projectionMatrix = Camera.main.GetStereoProjectionMatrix(Camera.StereoscopicEye.Right);
+            secondCamera.targetTexture = rt_right;
+            secondCamera.Render();
+        }
+        finally
+        {
+            secondCamera.transform.SetPositionAndRotation(sc_pos, sc_rot);
+        }
+    }
+}
---
+++ Assets/WindowToElsewhere/WindowToElsewhere.shader
@@ -0,0 +1,56 @@
+﻿Shader "Custom/WindowToElsewhere"
+{
+	Properties
+	{
+		_LeftTex ("Texture", 2D) = "white" {}
+        _RightTex("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _LeftTex, _RightTex;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = o.vertex;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+                float2 uv = i.uv.xy / i.uv.w;
+                uv = float2(1 + uv.x, 1 - uv.y) * 0.5;
+                fixed4 col;
+                if (unity_StereoEyeIndex == 0)
+                    col = tex2D(_LeftTex, uv);
+                else
+                    col = tex2D(_RightTex, uv);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
