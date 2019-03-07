54c56d27200ea8b58adbc67c3544e49e51a13c34
Armin Rigo <arigo@tunes.org>
2019-03-07 12:21:35 +0100

    Writing to a buffer from a pixel shader

---
+++ Assets/FastNormals/FastNormalsSetup.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FastNormalsSetup : MonoBehaviour
+{
+    public Material material;
+    /*public RenderTexture paintAccum;*/
+    public ComputeBuffer fieldbuf;
+
+
+    private IEnumerator Start()
+    {
+        /*paintAccum = new RenderTexture(32, 32, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        paintAccum.name = "_MainTexInternal";
+        paintAccum.enableRandomWrite = true;
+        paintAccum.Create();
+
+        material.SetTexture("_MainTexInternal", paintAccum);
+        Graphics.ClearRandomWriteTargets();
+        Graphics.SetRandomWriteTarget(4, paintAccum);*/
+
+        fieldbuf = new ComputeBuffer(100, sizeof(uint), ComputeBufferType.Default);
+        material.SetBuffer("Field", fieldbuf);
+
+        uint[] fdata = new uint[100];
+        while (true)
+        {
+            yield return new WaitForSeconds(0.4f);
+            Graphics.SetRandomWriteTarget(1, fieldbuf);
+            yield return new WaitForSeconds(0.1f);
+            fieldbuf.GetData(fdata);
+            string s = "";
+            for (int j = 0; j < 100; j++)
+            {
+                if ((j % 10) == 0)
+                    s += " ";
+                s += fdata[j].ToString();
+            }
+            Debug.Log(s);
+
+            for (int i = 0; i < 100; i++)
+                fdata[i] = 0;
+            fieldbuf.SetData(fdata);
+        }
+    }
+}
---
+++ Assets/FastNormals/minitest.shader
@@ -0,0 +1,52 @@
+﻿Shader "Unlit/minitest"
+{
+	Properties
+	{
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+            #pragma target 5.0
+            #pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+			};
+
+
+            RWStructuredBuffer<uint>	Field	: register(u1);
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = o.vertex.xy / o.vertex.w;
+                return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+                int2 xy = int2(i.uv * 5 + 5);
+                int index = xy.y * 10 + xy.x;
+                Field[index] = 1;
+				return float4(0, i.uv, 1);
+			}
+			ENDCG
+		}
+	}
+}
