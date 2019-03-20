eb3dcf6768fc4e2b5fddc6412283bdfbce85ff8e
Armin Rigo <arigo@tunes.org>
2019-03-20 09:49:49 +0100

    another experiment

---
+++ Assets/FastNormals/FNSetup2.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+
+public class FNSetup2 : MonoBehaviour
+{
+    /*public RenderTexture paintAccum;*/
+    public ComputeBuffer fieldbuf;
+
+
+    private IEnumerator Start()
+    {
+        int width = Display.main.renderingWidth;
+        int height = Display.main.renderingHeight;
+        Debug.Log(width + " x " + height);
+
+        fieldbuf = new ComputeBuffer(width * height, sizeof(uint), ComputeBufferType.Default);
+        Shader.SetGlobalBuffer("CustomFastNormals", fieldbuf);
+        Shader.SetGlobalVector("CustomFastNormals_Size", new Vector4(width, height, width * 0.5f, height * 0.5f));
+
+        uint[] fdata = new uint[width * height];
+        while (true)
+        {
+            yield return new WaitForSeconds(0.4f);
+            Graphics.SetRandomWriteTarget(1, fieldbuf);
+            yield return new WaitForSeconds(0.1f);
+            fieldbuf.GetData(fdata);
+            string s = "";
+            for (int j = 1; j < 10; j++)
+            {
+                int y = height * j / 10;
+                s += "-----";
+                for (int i = 1; i < 10; i++)
+                {
+                    int x = width * i / 10;
+                    uint norm = fdata[y * width + x];
+                    s += string.Format("[{0} {1}]", norm >> 16, norm & 0xffff);
+                }
+            }
+            Debug.Log(s);
+
+            /*uint max = 0;
+            for (int j = 1; j < fdata.Length; j++)
+                if (fdata[j] > max)
+                    max = fdata[j];
+            Debug.Log("MAX: " + max);*/
+
+            /*var file = new System.IO.FileStream("d:\\foo.txt", System.IO.FileMode.Create);
+            for (int i = 0; i < fdata.Length; i++)
+            {
+                uint norm = fdata[i];
+                string ss = string.Format("[{0} {1}]", norm >> 16, norm & 0xffff);
+                byte[] bb = Encoding.ASCII.GetBytes(ss);
+                file.Write(bb, 0, bb.Length);
+            }
+            file.Close();*/
+
+            //for (int i = 0; i < 100; i++)
+            //    fdata[i] = 0;
+            //fieldbuf.SetData(fdata);
+        }
+    }
+}
---
+++ Assets/FastNormals/minitest 2.shader
@@ -0,0 +1,59 @@
+﻿Shader "Unlit/minitest2"
+{
+	Properties
+	{
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+            #pragma target 5.0
+            #pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+                float3 normal : NORMAL;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                float3 normal : TEXCOORD1;
+			};
+
+
+            RWStructuredBuffer<uint> CustomFastNormals : register(u1);
+            float4 CustomFastNormals_Size;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = o.vertex.xy / o.vertex.w;
+                o.normal = COMPUTE_VIEW_NORMAL;
+                return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+                int2 xy = int2((i.uv + float2(1, 1)) * CustomFastNormals_Size.zw);
+                int index = xy.y * int(CustomFastNormals_Size.x) + xy.x;
+                float2 norm = EncodeViewNormalStereo(i.normal);
+                uint2 norm_int = uint2(norm * 32768);
+                CustomFastNormals[index] = norm_int.x * 65536 + norm_int.y;
+
+				return float4(1, i.uv, 1);
+			}
+			ENDCG
+		}
+	}
+}
