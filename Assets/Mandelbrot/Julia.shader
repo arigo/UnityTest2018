8dd374c68e76dab94160e8c7f0459edb26f17f15
Armin Rigo <arigo@tunes.org>
2019-05-18 19:01:48 +0200

    Check in an old Mandelbrot prototype

---
+++ Assets/Mandelbrot/Julia.shader
@@ -0,0 +1,67 @@
+﻿Shader "Unlit/Julia"
+{
+	Properties
+	{
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+            StructuredBuffer<float> Palette;
+            float4 C;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = (v.uv - float2(0.5, 0.5)) * 5;
+				return o;
+			}
+			
+			float4 frag (v2f ii) : SV_Target
+			{
+                float2 z = ii.uv;
+                //float2 z = float2(0, 0);
+                //return float4(z.x, z.y, 0, 1);
+
+                for (int i = 0; i < 85 * 2; i += 85)
+                {
+                    for (int j = 0; j < 85; j++)
+                    {
+                        /*  z => z * z + c  */
+                        z = float2(z.x * z.x - z.y * z.y, z.x * z.y * 2) + C.xy;
+                        
+                        if (dot(z, z) >= 4)
+                        {
+                            int k = (i + j) * 3;
+                            return float4(Palette[k], Palette[k + 1], Palette[k + 2], 1.0);
+                        }
+                    }
+                }
+                return float4(0, 0, 0, 1);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Mandelbrot/MandelbrotPalette.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BaroqueUI;
+
+
+public class MandelbrotPalette : MonoBehaviour
+{
+    Material mat;
+
+    readonly byte[][] colors1 = new byte[][] {
+        /*{8, 14, 32}, */
+        new byte[] {0, 0, 0},
+        new byte[] {120, 119, 238},
+        new byte[] {24, 7, 25},
+        new byte[] {197, 66, 28},
+        new byte[] {29, 18, 11},
+        new byte[] {135, 46, 71},
+        new byte[] {24, 27, 13},
+        new byte[] {241, 230, 128},
+        new byte[] {17, 31, 24},
+        new byte[] {240, 162, 139},
+        new byte[] {11, 4, 30},
+        new byte[] {106, 87, 189},
+        new byte[] {29, 21, 14},
+        new byte[] {12, 140, 118},
+        new byte[] {10, 6, 29},
+        new byte[] {50, 144, 77},
+        new byte[] {22, 0, 24},
+        new byte[] {148, 188, 243},
+        new byte[] {4, 32, 7},
+        new byte[] {231, 146, 14},
+        new byte[] {10, 13, 20},
+        new byte[] {184, 147, 68},
+        new byte[] {13, 28, 3},
+        new byte[] {169, 248, 152},
+        new byte[] {4, 0, 34},
+        new byte[] {62, 83, 48},
+        new byte[] {7, 21, 22},
+        new byte[] {152, 97, 184},
+        new byte[] {8, 3, 12},
+        new byte[] {247, 92, 235},
+        new byte[] {31, 32, 16}
+    };
+
+    const int MAX_ITER = 171;
+    ComputeBuffer cb;
+
+    void MakePalette()
+    {
+        var RGB = new List<float>();
+
+        for (int i = 0; i < MAX_ITER; i++)
+        {
+            byte[] inp = colors1[i % colors1.Length];
+            RGB.Add(inp[0] / 255f);
+            RGB.Add(inp[1] / 255f);
+            RGB.Add(inp[2] / 255f);
+        }
+
+        cb = new ComputeBuffer(170 * 3, 4);
+        cb.SetData(RGB);
+    }
+
+    private void Start()
+    {
+        MakePalette();
+        mat = GetComponent<Renderer>().material;   /* make a copy */
+    }
+
+    private void Update()
+    {
+        mat.SetBuffer("Palette", cb);
+
+        foreach (var ctrl in Controller.GetControllers())
+        {
+            if (ctrl.isActiveAndEnabled)
+            {
+                Vector3 pos = ctrl.position;
+                pos = transform.InverseTransformPoint(pos);
+                mat.SetVector("C", pos * 5f);
+                break;
+            }
+        }
+    }
+}
